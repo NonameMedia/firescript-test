df9bdad5a717d2a0af4117e9fee060cab0ced830
Andi Heinkelein <andifeind@noname-media.com>
2018-06-11 19:26:24 +0200

    Initial import

---
+++ test.js
@@ -0,0 +1,21 @@
+const Mocha = require('mocha')
+const fs = require('fs')
+const path = require('path')
+
+const mocha = new Mocha({
+  reporter: 'lagoon-reporter'
+})
+
+const testDir = path.join(__dirname, './tests/')
+
+fs.readdirSync(testDir).filter((file) => {
+  return file.endsWith('.spec.js')
+}).forEach((file) => {
+  mocha.addFile(
+    path.join(testDir, file)
+  )
+})
+
+mocha.run((failures) => {
+  process.exitCode = failures ? -1 : 0
+})
