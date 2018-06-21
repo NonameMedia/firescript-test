822980ecedc743f8c57bc1164db3f2463b90588e
Andi Heinkelein <andifeind@noname-media.com>
2018-06-21 20:02:26 +0200

    Add tests for typing and default params

---
+++ syntax/function-parameter-type-binding/result.js
@@ -0,0 +1,5 @@
+import { default as __FS } from 'firescript-runtime';
+function getBananas (amount) {
+  amount =  __FS.bindType(__FS.TYPE_STR, amount);
+  return 3;
+}
---
+++ syntax/variable-type-binding/result.js
@@ -0,0 +1,3 @@
+const __FS = require('firescript-runtime').default;
+const banana = __FS.bindType(__FS.TYPE_STR, 'Banana');
+module.exports.__esModule = true;
