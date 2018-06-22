dec56b75c3ed2587ea8f28d8ec5d62940be7c045
Andifeind <andifeind@noname-media.com>
2018-06-22 22:56:29 +0200

    Adjust tests

--- syntax/variable-type-binding/result.js
+++ syntax/variable-type-binding/result.js
@@ -1,3 +1,2 @@
-const __FS = require('firescript-runtime').default;
-const banana = __FS.bindType(__FS.TYPE_STR, 'Banana');
-module.exports.__esModule = true;
+import __FS from 'firescript-runtime';
+const banana = __FS.typing(__FS.TYPE_STR, 'Banana');
