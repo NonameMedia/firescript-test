6954302897726fe98021aa6a00548e7826e83e19
Andi Heinkelein <andifeind@noname-media.com>
2018-10-22 23:20:02 +0200

    Remove old stuff

--- syntax/function-parameter-typing/result.js
+++
@@ -1,5 +0,0 @@
-import __FS from 'firescript-runtime';
-function getBananas (amount) {
-  __FS.typing(__FS.TYPE_NUM, amount);
-  return 3;
-}
--- syntax/variable-type-binding/result.js
+++
@@ -1,2 +0,0 @@
-import __FS from 'firescript-runtime';
-const banana = __FS.typing(__FS.TYPE_STR, 'Banana');
